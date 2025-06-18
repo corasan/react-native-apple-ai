328b2784773fb54e73e53a37a20bcfa83ddf7f9f
Henry Paulino <me@henrypl.com>
2025-06-18 02:44:25 -0400

    Update ToolBridge.nitro.ts

--- package/src/specs/ToolBridge.nitro.ts
+++ package/src/specs/ToolBridge.nitro.ts
@@ -2,5 +2,4 @@ import type { AnyMap, HybridObject } from 'react-native-nitro-modules'
 
 export interface ToolBridge extends HybridObject<{ ios: 'swift' }> {
   registerJSFunction(name: string, implementation: () => AnyMap): void
-  // callJSFunction(functionName: string, args: Record<string, string>): void
 }
