abc59b4ff26c337f8767ad1a9cb5ff902212b40f
Henry Paulino <me@henrypl.com>
2025-06-26 00:36:11 -0400

    new files

---
+++ package/ios/HybridTool.swift
@@ -0,0 +1,20 @@
+import FoundationModels
+import NitroModules
+
+class HybridTool: HybridToolSpec, Tool, @unchecked Sendable {
+    let name: String
+    let description: String
+    var arguments: AnyMap
+    
+    init(name: String, description: String, arguments: AnyMap) {
+        self.name = name
+        self.description = description
+        self.arguments = arguments
+    }
+    
+    typealias Arguments = GeneratedContent
+    
+    func call(arguments: GeneratedContent) async throws -> ToolOutput {
+        return ToolOutput(GeneratedContent(properties: ["name": "Henry"]))
+    }
+}
---
+++ package/src/specs/Tool.nitro.ts
@@ -0,0 +1,8 @@
+import type { AnyMap, HybridObject } from 'react-native-nitro-modules'
+
+export interface Tool extends HybridObject<{ ios: 'swift' }> {
+  readonly name: string
+  readonly description: string
+  readonly arguments: AnyMap
+  call: Promise<AnyMap>
+}
