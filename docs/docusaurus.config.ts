558166043901730b57a23c1e9d3d5deab4a6d385
Henry Paulino <me@henrypl.com>
2025-09-18 01:50:58 -0400

    docs deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,15 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'corasan', // Usually your GitHub org/user name.
-  projectName: 'react-native-apple-ai', // Usually your repo name.
+  url: 'https://corasan.github.io',
+  baseUrl: '/react-native-apple-ai/',
+  organizationName: 'corasan',
+  projectName: 'react-native-apple-ai',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
